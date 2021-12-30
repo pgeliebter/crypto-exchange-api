bf2103073541af2e9a079c9c1baa9e59abf6f6f7
Pinny Geliebter <pgibralter@gmail.com>
2021-12-30 12:20:22 -0500

    binance with gets.chomp

--- binance.rb
+++ binance.rb
@@ -1,6 +1,16 @@
 require 'faraday'
 require 'json'
 
+# get trading pairs for binance.us
+symbols = JSON.parse(Faraday.get('https://api.binance.us/api/v3/exchangeInfo').body)['symbols']
+pp symbols
+
+puts "please type in a symbol"
+
+# symbol is in the form of "BTCUSDT" or "ETHBTC" or "ETHUSD"
+current_symbol = gets.chomp
+
 # call the Binance.us api with faraday
-response = Faraday.get("https://api.binance.us/api/v3/depth?symbol=#{BTCUSDT}&limit=20")
+response = Faraday.get("https://api.binance.us/api/v3/depth?symbol=#{current_symbol}&limit=20")
+
 pp JSON.parse(response.body)
